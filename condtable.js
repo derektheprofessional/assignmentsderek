511dbd622f3d2e612cae79a164bc93fe0948d568
Derek Houston <derektheprofessional@gmail.com>
2013-10-28 20:34:22 -0400

    yooo

---
+++ condtable.js
@@ -0,0 +1,16 @@
+n = 5
+if (n > 5) {
+    if (n >10) {
+        if (n > 15) {
+            if (n > 20) {
+                if (n > 25) {
+                    return "Woah thats a big ass num-num!";
+                }
+                return "Woah thats a decent sized num-num!";
+            }
+            return "Okay, that num-num is big. Not too big but big.";
+        }
+        return "Hmmm cool number but kind of small..";
+    }
+    return "Haha that num-num is a baby num-num!";
+}
